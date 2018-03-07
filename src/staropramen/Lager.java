8a6b56b5060a9aa599f76d6fc70f010701c6169e
misterHartman <richard_hartman_@hotmail.com>
2018-03-07 20:50:51 +0100

    my first commit

---
+++ src/staropramen/Lager.java
@@ -0,0 +1,19 @@
+package staropramen;
+
+import java.util.Scanner;
+
+public class Lager {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("What is your name?");
+		String name = scan.nextLine();
+		
+		System.out.println("Your name is " + name);
+		
+		scan.close();
+		
+	}
+}
